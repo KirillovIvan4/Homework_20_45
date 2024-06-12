93eefad036b156a43c8b4d61b5fbfbf6218300c3
Kirillov_Ivan <kirillovio@mail.ru>
2024-06-12 16:30:14 +0300

    finish

--- catalog/views.py
+++ catalog/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-
+def contacts(request):
+    if request. method == 'POST':
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        massage = request.POST.get("massage")
+        print(f'{name} ({phone}): {massage}')
+    return render(request, 'contacts.html')
