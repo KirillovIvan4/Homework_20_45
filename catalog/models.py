ffd3335d21f1746cdda3d7b83cdb41fa523aec8f
Kirillov_Ivan <kirillovio@mail.ru>
2024-06-17 17:36:59 +0300

    добавил миграции

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+NULLBLE = {"blank": True, "null": True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="имя")
+    description = models.TextField(verbose_name="описание")
+    preview = models.ImageField(upload_to="products/", verbose_name="превью")
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        related_name="product",
+        verbose_name="категория",
+        **NULLBLE,
+    )
+    purchase_price = models.IntegerField(verbose_name="цена покупки")
+    created_at = models.DateTimeField(verbose_name="дата создания ")
+    updated_at = models.DateTimeField(verbose_name="дата последних изменени")
+
+    def __str__(self):
+        return f"{self.name} {self.category}"
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ["name", "purchase_price"]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="имя")
+    description = models.CharField(max_length=10000, verbose_name="описание")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = [
+            "name",
+        ]
